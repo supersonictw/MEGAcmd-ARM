7a3b198c021e1f9a1fbc9d44100a8463c5eeb251
SuperSonic <supersonic@livemail.tw>
2023-03-16 16:00:39 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# megacmd_arm32v6_musl
+# (c) 2023 SuperSonic (https://github.com/supersonictw).
+
+set -e
+
+apk add --update --no-cache \
+    c-ares \
+    sqlite-libs \
+    sqlite \
+    pcre \
+    libcurl \
+    libtool \
+    libuv \
+    libpcrecpp \
+    libsodium \
+    readline \
+    freeimage \
+    zlib
+
+apk add --update --no-cache --virtual .build-deps \
+    autoconf \
+    automake \
+    c-ares-dev \
+    curl \
+    curl-dev \
+    file \
+    g++ \
+    gcc \
+    git \
+    make \
+    openssl \
+    openssl-dev \
+    zlib-dev \
+    readline-dev \
+    sqlite-dev \
+    pcre-dev \
+    libc-dev \
+    libffi-dev \
+    libsodium \
+    libsodium-dev \
+    libuv-dev \
+    freeimage-dev
+
+apk add --repository https://dl-cdn.alpinelinux.org/alpine/edge/testing --update --no-cache \
+    crypto++ \
+    crypto++-dev
+
+git clone --recursive https://github.com/meganz/MEGAcmd.git /tmp/MEGAcmd
+cd /tmp/MEGAcmd
+
+sh autogen.sh
+./configure --host=armv6-alpine-linux-musleabihf
+make -j $(nproc)
+
+# clean:
+# apk del .build-deps crypto++-dev
